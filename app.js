3606ac514ba7f3e635280919c81478f8b32ecebd
Jack Gamboa <jgamboa929@gmail.com>
2018-10-12 15:42:14 -0500

    To Do List with jQuery

---
+++ app.js
@@ -0,0 +1,34 @@
+
+
+let txtTask = $("#txtTask");
+let btnAdd = $("#btnAdd");
+let ulListPending = $("#ulListPending");
+let ulListComplete = $("#ulListComplete");
+
+
+
+btnAdd.click(function(){
+    let txtTaskValue = txtTask.val();
+
+    let liItem = $(`<li><input type="checkbox" onchange="checkBoxChange(this)">${txtTaskValue}<button type="button" onclick="removeLiItem(this)">Remove</button></li>`);
+
+    $("#ulListPending").append(liItem);
+
+});
+
+
+function checkBoxChange(sender) {
+    if (sender.checked) {
+        $("#ulListComplete").append(sender.parentNode);
+    }
+    else {
+        $("#ulListPending").append(sender.parentNode);
+    }
+}
+
+
+function removeLiItem(sender) {
+    let liParent = sender.parentNode
+    liParent.parentNode.removeChild(sender.parentNode)
+}
+
